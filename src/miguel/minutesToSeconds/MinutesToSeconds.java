da432abf7799bc243e2d43b4b12400880e6f5691
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-12-06 14:37:33 -0600

    Completed 3rd pod

--- src/miguel/minutesToSeconds/MinutesToSeconds.java
+++ src/miguel/minutesToSeconds/MinutesToSeconds.java
@@ -1,4 +1,14 @@
 package miguel.minutesToSeconds;
 
 public class MinutesToSeconds {
+    public static int convert(int minutes) {
+        return minutes * 60;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(convert(6));
+        System.out.println(convert(4));
+        System.out.println(convert(8));
+        System.out.println(convert(60));
+    }
 }
